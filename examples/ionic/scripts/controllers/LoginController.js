afcfab59281b9bbf5de1b3e0925ae6c8897aec45
Deny Pătrașcu <denypatrascu@gmail.com>
2021-01-12 16:24:31 +0200

    Binding simplification

--- examples/ionic/scripts/controllers/LoginController.js
+++ examples/ionic/scripts/controllers/LoginController.js
@@ -44,6 +44,65 @@ class LoginController extends CardinalController {
                     </ion-button>`,
                 slot: 'end'
             }
+        },
+        content: {
+            intro: {
+                innerHTML: `<ion-label class="intro">Stay in touch with Us!</ion-label>`,
+                color: 'primary'
+            },
+            email: {
+                icon: {
+                    name: 'person-circle',
+                    slot: 'start'
+                },
+                input: {
+                    value: 'john.doe@example.com',
+                    placeholder: 'EMAIL',
+                    type: 'email',
+                    inputmode: 'email',
+                    required: true
+                }
+            },
+            password: {
+                icon: {
+                    name: 'lock-closed',
+                    slot: 'start'
+                },
+                input: {
+                    value: 'test1234',
+                    placeholder: 'PASSWORD',
+                    type: 'password',
+                    required: true
+                },
+                show: {
+                    id: 'view-password',
+                    name: 'eye-outline',
+                    slot: 'end',
+                    style: 'cursor: pointer'
+                },
+                hide: {
+                    name: 'eye-off-outline'
+                }
+            },
+            'remember-me': {
+                innerHTML:
+                    `<ion-checkbox slot="start" checked></ion-checkbox>
+                    <ion-label>Remember me!</ion-label>`,
+                lines: 'none',
+                style: 'padding-bottom: 2em'
+            }
+        },
+        footer: {
+            login: {
+                expand: 'block',
+                style: 'padding: 0 1em',
+                id: 'login',
+                innerHTML: 'Login <ion-icon slot="end" name="log-in"></ion-icon>'
+            },
+            register: {
+                innerText: `I don't have an account yet!`,
+                href: '/register'
+            }
         }
     })
 
@@ -73,12 +132,46 @@ class LoginController extends CardinalController {
         location.pathname = '/demo';
     }
 
+    async showPassword(event) {
+        event.stopImmediatePropagation();
+        event.target.parentElement.children[1].type = 'text';
+        event.target.setAttribute('name', this.model.main.password.hide.name);
+    }
+
+    async hidePassword(event) {
+        event.stopImmediatePropagation();
+        event.target.parentElement.children[1].type = 'password';
+        event.target.setAttribute('name', this.model.main.password.show.name);
+    }
+
+    async togglePassword(event) {
+        if (event.target.getAttribute('name') === this.model.main.password.hide.name) {
+            await this.hidePassword(event);
+        } else {
+            await this.showPassword(event);
+        }
+    }
+
     async handleOnReady() {
         const menuElement = this.element.querySelector('#menu');
         const loginButtonElement = this.element.querySelector('#login');
+        const viewElement = this.element.querySelector('#view-password');
 
         menuElement.addEventListener('click', this.handleMenuClick.bind(this));
         loginButtonElement.addEventListener('click', this.handleLoginClick.bind(this));
+
+        if ('ontouchstart' in window || navigator.msMaxTouchPoints > 0) {
+            viewElement.addEventListener('touchstart', this.togglePassword.bind(this))
+        } else {
+            viewElement.addEventListener('mousedown', this.showPassword.bind(this));
+            viewElement.addEventListener('mouseup', this.hidePassword.bind(this));
+        }
+
+        // TODO: two-way binding
+        // let value = 0;
+        // loginButtonElement.addEventListener('click', () => {
+        //     this.model.header.title = `${++value}`;
+        // })
     }
 
     constructor(element, history) {
@@ -86,8 +179,9 @@ class LoginController extends CardinalController {
 
         this.model = this.setModel(this.getModel());
 
+        // <wcc-bindable> loaded
         element.componentOnReady().then(this.handleOnReady.bind(this));
     }
 }
 
-export default LoginController;
+export default LoginController; // used by <wcc-bindable>
